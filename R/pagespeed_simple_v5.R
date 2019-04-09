daf2ce45a105c4558587a4d1b59529b9f7bb7839
Leszek-Sieminski <32634801+Leszek-Sieminski@users.noreply.github.com>
2019-04-09 11:11:36 +0200

    delete unnecessary testing code inside function

--- R/pagespeed_simple_v5.R
+++ R/pagespeed_simple_v5.R
@@ -34,16 +34,6 @@ pagespeed_simple_v5 <- function(url, key = Sys.getenv("PAGESPEED_API_KEY"),
                                 enhanced_lighthouse = FALSE, locale = NULL,
                                 utm_campaign = NULL, utm_source = NULL)
 {
-  url = "http://www.rennie.pl"
-  key = Sys.getenv("PAGESPEED_API_KEY")
-  strategy = NULL
-  categories = "performance"
-  interval = 0.5
-  enhanced_lighthouse = FALSE
-  locale = NULL
-  utm_campaign = NULL
-  utm_source = NULL
-
   # safety net ----------------------------------------------------------------
   if (is.null(key) | nchar(key) == 0){
     stop("API key is a NULL or has length = 0. Please check it and provide a proper API key.", call. = FALSE)}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# pagespeedParseR  v0.2.0
+# pagespeedParseR  v0.2.1
 R wrapper for Google Pagespeed Insights API
 
 ![Lifecycle_Status](https://img.shields.io/badge/lifecycle-maturing-blue.svg)
