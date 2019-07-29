b4512d7bd1d8f107619e1e5c1396d314692898f1
Leszek-Sieminski <32634801+Leszek-Sieminski@users.noreply.github.com>
2019-07-29 16:30:02 +0200

    fix largeList class problem

--- R/fun_lh_cat_id.R
+++ R/fun_lh_cat_id.R
@@ -29,7 +29,7 @@ fun_lh_cat_id <- function(id, parsed) {
     length(id) == 1,
     id %in% c("performance", "accessibility", "best-practices", "seo", "pwa"),
     !(is.na(parsed) || is.null(parsed)),
-    is.list(parsed),
+    is.list(parsed) || class(parsed) == "largeList",
     !is.data.frame(parsed)
     # length(parsed) >
   )
